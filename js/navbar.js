9b04535ac673a35ddedbb5131ea290faa5f903d6
iamtashi <tacmoktan@gmail.com>
2019-09-12 15:07:37 +0545

    Add logic for dynamic logo size

--- js/navbar.js
+++ js/navbar.js
@@ -2,22 +2,21 @@ const headerHeight = 120;
 
 //elements
 const header = document.querySelector('#header');
-let anchors = document.querySelectorAll('#nav-links a');
-const logoImage = document.querySelector('#logo')
-
+const logoImg = document.querySelector('#logo a img');
+console.log(logoImg);
 window.onscroll = () => {
 
     if (window.pageYOffset > 5) {
 
         header.style.background = "white";
-        anchors.forEach(anchor => {
-            anchor.style.color = "black";
-        });
-
+        header.style.boxShadow = "0px 1px 5px black";
+        header.style.transition = "box-shadow 0.15s ease-in";
+        logoImg.style.height= 60;
+        
     } else {
 
         header.style.background = "transparent";
-        anchors.forEach(anchor => anchor.style.color = "white");
-
+        header.style.boxShadow = "none";
+        logoImg.style.height=100;
     }
 }
