cb25dce0e0f173c56fb4d48b8f53406fb554be46
iamtashi <tacmoktan@gmail.com>
2019-09-13 17:49:41 +0545

    Add responsive navbar

--- js/navbar.js
+++ js/navbar.js
@@ -11,12 +11,24 @@ window.onscroll = () => {
         header.style.background = "white";
         header.style.boxShadow = "0px 1px 5px black";
         header.style.transition = "box-shadow 0.15s ease-in";
-        logoImg.style.height= 60;
-        
+        logoImg.style.height = 60;
+
     } else {
 
         header.style.background = "transparent";
         header.style.boxShadow = "none";
-        logoImg.style.height=100;
+        logoImg.style.height = 100;
     }
 }
+
+//responsive navbar
+const hamburger = document.querySelector('#hamburger');
+const drawer = document.querySelector('#drawer');
+
+window.onclick = event => {
+    if (event.target === hamburger) 
+        drawer.style.right = 0;
+    else 
+        drawer.style.right = -210;
+}
+
